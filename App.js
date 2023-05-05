b4c0e19ca737ac5d16f71a5ac71a0691a1346d9f
itbintangtoedjoe <82027566+itbintangtoedjoe@users.noreply.github.com>
2023-05-05 09:51:19 +0700

    standalones

---
+++ App.js
@@ -0,0 +1,258 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useEffect} from 'react';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text,
+  BackHandler,
+  Alert,
+  Image,
+  ActivityIndicator,
+  Platform,
+} from 'react-native';
+import {Provider} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
+import {ToastProvider} from 'react-native-toast-notifications';
+import {useSelector, useDispatch} from 'react-redux';
+
+import Colors from './src/general/constants/Colors';
+import MilliardText from './src/general/components/MilliardText';
+import BaseNavigator from './src/general/navigation';
+import {store} from './src/general/redux/store';
+import NavigationService from './src/general/NavigationService';
+import RemotePushController from './src/general/services/RemotePushController';
+import * as generalAction from './src/general/redux/actions/generalAction';
+import {version} from './package.json';
+
+console.reportErrorsAsExceptions = false;
+
+const App = () => {
+  ////////////////CHECK VERSION////////////////
+  // const latestAppVersion = useSelector(state => state.general.latestAppVersion);
+  // const checkingVerLoadingState = useSelector(
+  //   state => state.general.checkingVerLoadingState,
+  // );
+
+  // const dispatch = useDispatch();
+
+  // const VersionCheckMessage = () => {
+  //   if (Platform.OS === 'ios') {
+  //     SplashScreen.hide();
+  //     return (
+  //       <ToastProvider
+  //         successColor={Colors.primaryColor}
+  //         textStyle={{
+  //           fontFamily: 'Milliard-Book',
+  //         }}
+  //         offsetBottom={40}>
+  //         {/* <Provider store={store}> */}
+  //         <BaseNavigator
+  //           ref={navigatorRef => {
+  //             NavigationService.setTopLevelNavigator(navigatorRef);
+  //           }}
+  //         />
+  //         {/* </Provider> */}
+  //         {/* <RemotePushController /> */}
+  //       </ToastProvider>
+  //     );
+  //   } else {
+  //     if (checkingVerLoadingState) {
+  //       console.log('still checking');
+  //       return (
+  //         <View style={styles.content}>
+  //           <ActivityIndicator size="small" color={Colors.primaryColor} />
+  //         </View>
+  //       );
+  //     }
+  //     if (
+  //       latestAppVersion == undefined ||
+  //       latestAppVersion == null ||
+  //       latestAppVersion == ''
+  //     ) {
+  //       console.log('masih kosong');
+  //       return null;
+  //     } else if (version == latestAppVersion.LatestVersion) {
+  //       console.log('up to date(y)');
+  //       SplashScreen.hide();
+  //       if (Platform.OS === 'android') {
+  //         Alert.alert(
+  //           'Youre good to go',
+  //           'Please update on the Play Store to access the app',
+  //         );
+  //       } else {
+  //         Alert.alert('Youre good to go');
+  //       }
+  //       return (
+  //         <ToastProvider
+  //           successColor={Colors.primaryColor}
+  //           textStyle={{
+  //             fontFamily: 'Milliard-Book',
+  //           }}
+  //           offsetBottom={40}>
+  //           {/* <Provider store={store}> */}
+  //           <BaseNavigator
+  //             ref={navigatorRef => {
+  //               NavigationService.setTopLevelNavigator(navigatorRef);
+  //             }}
+  //           />
+  //           {/* </Provider> */}
+  //           {/* <RemotePushController /> */}
+  //         </ToastProvider>
+  //       );
+  //     } else {
+  //       console.log('needs an update');
+  //       if (latestAppVersion.IsMandatory === true) {
+  //         if (Platform.OS === 'android') {
+  //           Alert.alert(
+  //             'B7 Connect needs an update',
+  //             'Please update on the Play Store to access the app',
+  //             [],
+  //           );
+  //           setTimeout(() => {
+  //             BackHandler.exitApp();
+  //           }, 1500);
+  //         } else {
+  //           Alert.alert('B7 Connect needs an update');
+  //         }
+  //         // SplashScreen.show();
+  //         return null;
+  //       } else {
+  //         Alert.alert(
+  //           "There's an update available",
+  //           'Update on the Play Store to access the newest features',
+  //         );
+  //         SplashScreen.hide();
+  //         return (
+  //           <ToastProvider
+  //             successColor={Colors.primaryColor}
+  //             textStyle={{
+  //               fontFamily: 'Milliard-Book',
+  //             }}
+  //             offsetBottom={40}>
+  //             {/* <Provider store={store}> */}
+  //             <BaseNavigator
+  //               ref={navigatorRef => {
+  //                 NavigationService.setTopLevelNavigator(navigatorRef);
+  //               }}
+  //             />
+  //             {/* </Provider> */}
+  //             {/* <RemotePushController /> */}
+  //           </ToastProvider>
+  //         );
+  //       }
+  //     }
+  //     // return <CarouselCards data={appBanners} />;
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   //check app ver
+  //   const data = {
+  //     appVersion: version,
+  //   };
+  //   dispatch(generalAction.checkAppVersion(data));
+  // }, [dispatch]);
+  ////////////////CHECK VERSION////////////////
+
+  // if (checkingVerLoadingState) {
+  //   return (
+  //     <View style={styles.content}>
+  //       <ActivityIndicator size="small" color={Colors.primaryColor} />
+  //     </View>
+  //   );
+  // } else if (!checkingVerLoadingState) {
+  //   console.log('curr ver: ' + version);
+  //   console.log('latest ver: ' + latestAppVersion);
+  //   console.log(version == latestAppVersion);
+  //   if (version == latestAppVersion) {
+  //     SplashScreen.hide();
+  //     Alert.alert(
+  //       'Youre good to go',
+  //       'Please update on the Play Store to access the app',
+  //     );
+  //   } else {
+  //     Alert.alert(
+  //       'B7 Connect needs an update',
+  //       'Please update on the Play Store to access the app',
+  //     );
+  //     // SplashScreen.show();
+  //     return null;
+  //   }
+  // }
+
+  // if (checkingVerLoadingState) {
+  //   return (
+  //     <View style={styles.content}>
+  //       <MilliardText style={{marginBottom: 10}}>
+  //         Please wait while we're checking your app version
+  //       </MilliardText>
+  //       <ActivityIndicator size="small" color={Colors.primaryColor} />
+  //     </View>
+  //   );
+  // }
+
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+  // useEffect(() => {
+  //   const backAction = () => {
+  //     Alert.alert(
+  //       'Exit B7 All-In-One?',
+  //       'Anda yakin keluar dari aplikasi TARA?',
+  //       [
+  //         {
+  //           text: 'Batalkan',
+  //           onPress: () => null,
+  //           style: 'cancel',
+  //         },
+  //         {text: 'Ya', onPress: () => BackHandler.exitApp()},
+  //       ],
+  //     );
+  //     return true;
+  //   };
+  //   const backHandler = BackHandler.addEventListener(
+  //     'hardwareBackPress',
+  //     backAction,
+  //   );
+
+  //   return () => backHandler.remove();
+  // });
+
+  return (
+    <ToastProvider
+      successColor={Colors.primaryColor}
+      textStyle={{
+        fontFamily: 'Milliard-Book',
+      }}
+      offsetBottom={40}>
+      {/* <Provider store={store}> */}
+      <BaseNavigator
+        ref={navigatorRef => {
+          NavigationService.setTopLevelNavigator(navigatorRef);
+        }}
+      />
+      {/* </Provider> */}
+      {/* <RemotePushController /> */}
+    </ToastProvider>
+  );
+
+  // return <VersionCheckMessage />;
+};
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import {Provider} from 'react-redux';
+import PushNotification, {Importance} from 'react-native-push-notification';
+
+import {store} from './src/general/redux/store';
+
+const ReduxProvider = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => ReduxProvider);
+
+PushNotification.createChannel(
+  {
+    channelId: 'b7con-chan-id', // (required)
+    channelName: 'Notif Channel', // (required)
+    channelDescription: 'A channel to categorise your notifications', // (optional) default: undefined.
+    playSound: false, // (optional) default: true
+    soundName: 'default', // (optional) See `soundName` parameter of `localNotification` function
+    importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
+    vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
+  },
+  created => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+);
